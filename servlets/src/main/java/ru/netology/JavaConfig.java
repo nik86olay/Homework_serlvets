40f787053c2bb9b6c47cc6cb3b1dfbd3d326e2c8
nik86olay <nik86olaygmail.com>
2023-04-04 22:38:43 +0300

    MainServletCorrect Commit

--- servlets/src/main/java/ru/netology/JavaConfig.java
+++ servlets/src/main/java/ru/netology/JavaConfig.java
@@ -1,7 +1,24 @@
 package ru.netology;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
 @Configuration
 public class JavaConfig {
     @Bean
+    public PostController postController(){
+        return new PostController(postService());
+    }
+    @Bean
+    public PostService postService(){
+        return new PostService(postRepository());
+    }
+
+    private PostRepository postRepository() {
+        return new PostRepository();
+    }
 
 }
--- servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,5 +1,6 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.netology.JavaConfig;
 import ru.netology.controller.PostController;
 import ru.netology.repository.PostRepository;
